e0c5925aca63463b643de1d57b0afcd51d955831
Lucas Ezequiel Mariano Arenaz <LArenaz@bpba.com.ar>
2025-07-03 03:21:54 -0300

    ok

--- src/components/ProfileScreen.js
+++ src/components/ProfileScreen.js
@@ -13,6 +13,7 @@ const ProfileScreen = ({ onGoBack, canGoBack }) => {
       {/* Header */}
       <div className="flex justify-between items-center mb-8">
         {canGoBack ? (
+          
           <button 
             onClick={onGoBack}
             className="w-6 h-6 text-gray-600 hover:text-gray-800 transition-colors"
